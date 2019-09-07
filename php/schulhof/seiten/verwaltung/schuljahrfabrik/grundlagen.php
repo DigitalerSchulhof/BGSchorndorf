62464a98c8b28034220daded1dbe4eb3216427a5
Patrick Wagner <pawa@matinf.de>
2019-09-07 17:57:46 +0200

    [FIX] Typo

--- php/schulhof/seiten/verwaltung/schuljahrfabrik/grundlagen.php
+++ php/schulhof/seiten/verwaltung/schuljahrfabrik/grundlagen.php
@@ -149,7 +149,7 @@ if ($zugriff) {
       if ($SJFG == "Stufen") {
         $sql = $dbs->prepare("SELECT * FROM (SELECT id, AES_DECRYPT(bezeichnung, '$CMS_SCHLUESSEL') AS bezeichnung FROM $kSJFG WHERE schuljahr = ? ORDER BY reihenfolge) AS s");
       }
-      else if ($SFJG == "Klassen") {
+      else if ($SJFG == "Klassen") {
         $sql = $dbs->prepare("SELECT * FROM (SELECT klassen.id, AES_DECRYPT(klassen.bezeichnung, '$CMS_SCHLUESSEL') AS bezeichnung FROM $kSJFG JOIN stufen ON klassen.stufe = stufen.id WHERE klassen.schuljahr = ? ORDER BY reihenfolge) AS s");
       }
       else {
