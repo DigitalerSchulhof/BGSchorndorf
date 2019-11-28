c72ce4cbc64032a3dca301f38cd5b34529234a19
jeengbe <jesper.engberg@gmx.at>
2019-11-28 18:15:16 +0100

    [Fix] Galerien besetzen Pfeiltasten

--- js/website/galerien.js
+++ js/website/galerien.js
@@ -1,30 +1,33 @@
 $(window).on("load", function() {
-  galerie.zeigen(0);
-  galerie.starteLoop();
-  galerie.keydownSetzen();
+  if($("#cms_hauptteil_i>#cms_galerie_bilder").length) {
+    galerie.zeigen(0);
+    galerie.starteLoop();
+    galerie.keydownSetzen();
+  }
 });
 
 var galerie = {
   index: null,
   interval: null,
   zeigen: function(i, auto) {
-    if(i.jQuery)
+    if(i.jQuery) {
       if(!i.parents("#cms_galerie_bilder").length || !i.is(".cms_galerie_bild"))
         return false;
       else
         i = i.index(".cms_galerie_bild");
+    }
 
-      $(".cms_galerie_bild").removeClass("cms_galerie_zeigen");
-      galerie.index = i;
-      galerie.dotZeigen();
-      if(!auto) {
-        clearInterval(galerie.interval);
-        galerie.interval = null;
-        setTimeout(function() {
-          galerie.starteLoop();
-        }, 6000); // Bei manuellem Wechsel 6s warten
-      }
-      return $("#cms_galerie_bilder").find(".cms_galerie_bild:eq("+i+")").addClass("cms_galerie_zeigen");
+    $(".cms_galerie_bild").removeClass("cms_galerie_zeigen");
+    galerie.index = i;
+    galerie.dotZeigen();
+    if(!auto) {
+      clearInterval(galerie.interval);
+      galerie.interval = null;
+      setTimeout(function() {
+        galerie.starteLoop();
+      }, 6000); // Bei manuellen Wechsel 6s warten
+    }
+    return $("#cms_galerie_bilder").find(".cms_galerie_bild:eq("+i+")").addClass("cms_galerie_zeigen");
   },
   dotZeigen: function(i) {
     i !== 0 && (i = i || galerie.index);
