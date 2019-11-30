6b2f6aa2403f2ac52109f0a599a5b0e4d2ffa7df
jeengbe <jesper.engberg@gmx.at>
2019-11-30 19:07:24 +0100

    [Fix] Favoriten sortieren nach ID (=Zeitpunkt) -  Favoriten umbennen Knopf vereinheitlicht

--- php/schulhof/seiten/nutzerkonto/favoriten.php
+++ php/schulhof/seiten/nutzerkonto/favoriten.php
@@ -4,21 +4,21 @@
 <h1>Favoriten</h1>
 <?php
 $tabellencode = "";
-$sql = $dbs->prepare("SELECT * FROM (SELECT id, AES_DECRYPT(bezeichnung, '$CMS_SCHLUESSEL') AS bezeichnung, AES_DECRYPT(url, '$CMS_SCHLUESSEL') AS url FROM favoritseiten WHERE person = ?) AS x ORDER BY bezeichnung, url");
+$sql = $dbs->prepare("SELECT * FROM (SELECT id, AES_DECRYPT(bezeichnung, '$CMS_SCHLUESSEL') AS bezeichnung, AES_DECRYPT(url, '$CMS_SCHLUESSEL') AS url FROM favoritseiten WHERE person = ?) AS x ORDER BY id");
 $sql->bind_param("i", $CMS_BENUTZERID);
 if ($sql->execute()) {
 	$sql->bind_result($fid, $fbez, $furl);
 	while ($sql->fetch()) {
 		$tabellencode .= "<tr><td><img src=\"res/icons/klein/favorit.png\"></td>";
+		$tabellencode .= "<td>$furl</td>";
 		$tabellencode .= "<td><input type=\"text\" name=\"cms_favoriten_bezeichnung_$fid\" id=\"cms_favoriten_bezeichnung_$fid\" value=\"$fbez\"></td>";
-		$tabellencode .= "<td><span class=\"cms_button\" onclick=\"cms_favorit_benennen('$fid')\">Umbenennen</span></td>";
-		$tabellencode .= "<td>$furl</td></tr>";
+		$tabellencode .= "<td><span class=\"cms_aktion_klein\" onclick=\"cms_favorit_benennen('$fid')\"><span class=\"cms_hinweis\">Umbenennen</span><img src=\"res/icons/klein/umbennen.png\"></span></td></tr>";
 	}
 }
 $sql->close();
 
 $code = "<table class=\"cms_liste\">";
-	$code .= "<tr><th></th><th>Bezeichnung</th><th></th><th>Zielseite</th></tr>";
+	$code .= "<tr><th></th><th>Zielseite</th><th>Bezeichnung</th><th></th></tr>";
 	if (strlen($tabellencode) > 0) {$code .= $tabellencode;}
 	else {$code .= "<tr><td colspan=\"4\" class=\"cms_notiz\">– keine Favoriten angelegt –</td></tr>";}
 $code .= "</table>";
