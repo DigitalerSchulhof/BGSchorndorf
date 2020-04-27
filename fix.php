0d2d7e4d8c92378e85845e65babe467563a359d9
oxydon <pawa@matinf.de>
2020-04-27 11:59:22 +0200

    [FIX] Versionierung und Speicherstatistik

--- fix.php
+++ fix.php
@@ -5,11 +5,53 @@ include_once("php/schulhof/funktionen/generieren.php");
 include_once("php/schulhof/funktionen/texttrafo.php");
 include_once("php/schulhof/anfragen/verwaltung/gruppen/initial.php");
 
-foreach ($CMS_GRUPPEN AS $g) {
-  $gk = cms_textzudb($g);
-  echo "CREATE TABLE `{$gk}todoartikel` ( `person` BIGINT(255) UNSIGNED NULL DEFAULT NULL , `blogid` BIGINT(255) UNSIGNED NULL DEFAULT NULL , `terminid` BIGINT(255) UNSIGNED NULL DEFAULT NULL ) ENGINE = InnoDB;
-  ALTER TABLE `{$gk}todoartikel` ADD UNIQUE `{$gk}todoartikelindex` (`person`, `blogid`, `terminid`);
-  ALTER TABLE `{$gk}todoartikel` ADD CONSTRAINT `{$gk}todoartikelblogblogeintrag` FOREIGN KEY (`blogid`) REFERENCES `{$gk}blogeintraegeintern`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-  ALTER TABLE `{$gk}todoartikel` ADD CONSTRAINT `{$gk}todoartikeltermintermin` FOREIGN KEY (`terminid`) REFERENCES `{$gk}termineintern`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;<br>";
+$dbs = cms_verbinden('s');
+$anzahl = 0;
+$sql = $dbs->prepare("SELECT * FROM (SELECT AES_DECRYPT(vorname, '$CMS_SCHLUESSEL') AS vorname, AES_DECRYPT(nachname, '$CMS_SCHLUESSEL') AS nachname, bedarf, AES_DECRYPT(betrag, '$CMS_SCHLUESSEL') AS preis, AES_DECRYPT(telefon, '$CMS_SCHLUESSEL') AS telefon, AES_DECRYPT(emailadresse, '$CMS_SCHLUESSEL') AS emailadresse, AES_DECRYPT(klassen.bezeichnung, '$CMS_SCHLUESSEL') AS kbez, eingegangen FROM ebedarf JOIN personen ON ebedarf.id = personen.id LEFT JOIN (SELECT klassen.id AS id, bezeichnung, person FROM klassen JOIN klassenmitglieder ON klassen.id = klassenmitglieder.gruppe WHERE schuljahr = 2) AS klassen ON klassen.person = personen.id WHERE personen.art = AES_ENCRYPT('s', '$CMS_SCHLUESSEL') AND bedarf > 0) AS x ORDER BY bedarf DESC, kbez ASC, nachname ASC, vorname ASC");
+if ($sql->execute()) {
+  $sql->bind_result($vor, $nach, $bedarf, $preis, $telefon, $email, $kbez, $eingegangen);
+  while ($sql->fetch()) {
+    echo $bedarf.";".$kbez.";".$nach.";".$vor.";".$preis.";Tel: ".$telefon.";".$email."<br>";
+    $anzahl++;
+  }
 }
+$sql->close();
+
+$sql = $dbs->prepare("SELECT * FROM (SELECT AES_DECRYPT(vorname, '$CMS_SCHLUESSEL') AS vorname, AES_DECRYPT(nachname, '$CMS_SCHLUESSEL') AS nachname, bedarf, AES_DECRYPT(betrag, '$CMS_SCHLUESSEL') AS preis, AES_DECRYPT(telefon, '$CMS_SCHLUESSEL') AS telefon, AES_DECRYPT(emailadresse, '$CMS_SCHLUESSEL') AS emailadresse, 'X' as kbez, eingegangen FROM ebedarf JOIN personen ON ebedarf.id = personen.id WHERE personen.art != AES_ENCRYPT('s', '$CMS_SCHLUESSEL') AND bedarf > 0) AS x ORDER BY bedarf DESC, kbez ASC, nachname ASC, vorname ASC");
+if ($sql->execute()) {
+  $sql->bind_result($vor, $nach, $bedarf, $preis, $telefon, $email, $kbez, $eingegangen);
+  while ($sql->fetch()) {
+    echo $bedarf.";".$kbez.";".$nach.";".$vor.";".$preis.";Tel: ".$telefon.";".$email."<br>";
+    $anzahl++;
+  }
+}
+$sql->close();
+
+$interesse = 0;
+$sql = $dbs->prepare("SELECT COUNT(*) FROM ebedarf WHERE bedarf != 0");
+if ($sql->execute()) {
+  $sql->bind_result($interesse);
+  $sql->fetch();
+}
+$sql->close();
+
+$nutzerkonten = 1;
+$sql = $dbs->prepare("SELECT COUNT(*) FROM nutzerkonten");
+if ($sql->execute()) {
+  $sql->bind_result($nutzerkonten);
+  $sql->fetch();
+}
+$sql->close();
+
+$sql = $dbs->prepare("SELECT COUNT(*) FROM ebedarf");
+if ($sql->execute()) {
+  $sql->bind_result($anzahl);
+  if ($sql->fetch()) {
+    echo "<br>".$interesse." Interessenten - ".$anzahl." Rückmeldungen - ".$nutzerkonten." Nutzerkonten - ".round($anzahl/$nutzerkonten*100,2)."%";
+  }
+}
+$sql->close();
+
+cms_trennen($dbs);
+
 ?>
