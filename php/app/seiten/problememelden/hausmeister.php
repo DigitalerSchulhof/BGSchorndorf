1818ed028865ab0664fee0ca2d49f886175cf614
jeengbe <jesper.engberg@gmx.at>
2020-12-20 15:30:04 +0100

    [Fix] Werbeslogan verbessert

--- php/app/seiten/problememelden/hausmeister.php
+++ php/app/seiten/problememelden/hausmeister.php
@@ -1,7 +1,7 @@
 <?php
 $code = "<h2>Hausmeisteraufträge erteilen</h2>";
 if (strpos($_SERVER['HTTP_USER_AGENT'], "dshApp") === false && strpos($_SERVER['HTTP_USER_AGENT'], "Android") !== false) {
-  $code .= cms_meldung("fehler", "<h4>Veraltete App!</h4><p>Es ist eine neue Version der App des Digitalen Schulhof verfügbar!<br>Diese kann <a href=\"https://play.google.com/store/apps/details?id=de.dsh\">hier</a> heruntergeladen werden.<br>Die alte Version kann und sollte anschließend deinstalliert werden.</p>");
+  $code .= cms_meldung("fehler", "<h4>Veraltete App!</h4><p>Es ist eine neue Version der App des Digitalen Schulhof verfügbar!<br>Diese kann <a href=\"https://play.google.com/store/apps/details?id=de.dsh\">hier</a> heruntergeladen werden. Neuerungen umfassen vereinfachte Navigation, Darkmode, direktes Öffnen von Seiten, erhöhte Stabilität und mehr!<br>Die alte Version kann und sollte anschließend deinstalliert werden.</p>");
 }
 
 include_once("php/schulhof/seiten/hausmeister/hausmeisterauftrag.php");
