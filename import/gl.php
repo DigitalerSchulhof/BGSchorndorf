afa91ca85c7aca3fd216d80f6be9dd2171ba91b8
jeengbe <jesper.engberg@gmx.at>
2021-01-27 09:36:34 +0100

    [Fix] Import alle Gruppen

--- import/gl.php
+++ import/gl.php
@@ -10,24 +10,28 @@
   $ins = [];
 
   $dbs = cms_verbinden("s");
-  $sql = $dbs->prepare("SELECT id, AES_DECRYPT(pinnwand, '$CMS_SCHLUESSEL') FROM sonstigegruppen WHERE LENGTH(AES_DECRYPT(pinnwand, '$CMS_SCHLUESSEL')) > 0 AND (SELECT COUNT(*) FROM sonstigegruppenlinks WHERE gruppe = id) = 0");
-  $sql->bind_result($gID, $gPW);
-  $sql->execute();
-  $sql->store_result();
-  while($sql->fetch()) {
-    if(preg_match("/https:\/\/elearning(\d)\.bg-schorndorf\.de\/(\D)\/([a-zA-Z]{3}-[a-zA-Z]{3}-[a-zA-Z]{3})/", $gPW, $link) === 1) {
-      array_shift($link);
-      $ins[] = [$gID, ...$link];
-    }
-  }
-  $sql->close();
-
-  $sql = $dbs->prepare("INSERT INTO sonstigegruppenlinks (id, gruppe, link, titel, beschreibung) VALUES (?, ?, AES_ENCRYPT(?, '$CMS_SCHLUESSEL'), AES_ENCRYPT('', '$CMS_SCHLUESSEL'), AES_ENCRYPT('', '$CMS_SCHLUESSEL'))");
-  $sql->bind_param("iis", $lID, $gID, $link);
-  foreach($ins as $i) {
-    $lID = cms_generiere_kleinste_id('sonstigegruppenlinks');
-    $gID = $i[0];
-    $link = "https://elearning{$i[1]}.bg-schorndorf.de/{$i[2]}/{$i[3]}";
+  foreach($CMS_GRUPPEN as $g) {
+    $gk = cms_textzudb($g);
+    $sql = $dbs->prepare("SELECT id, AES_DECRYPT(pinnwand, '$CMS_SCHLUESSEL') FROM {$gk} WHERE LENGTH(AES_DECRYPT(pinnwand, '$CMS_SCHLUESSEL')) > 0 AND (SELECT COUNT(*) FROM {$gk}links WHERE gruppe = id) = 0");
+    $sql->bind_result($gID, $gPW);
     $sql->execute();
+    $sql->store_result();
+    while($sql->fetch()) {
+      if(preg_match("/https:\/\/elearning(\d)\.bg-schorndorf\.de\/(\D)\/([a-zA-Z]{3}-[a-zA-Z]{3}-[a-zA-Z]{3})/", $gPW, $link) === 1) {
+        array_shift($link);
+        $ins[] = [$gID, ...$link];
+      }
+    }
+    $sql->close();
+
+    $sql = $dbs->prepare("INSERT INTO {$gk}links (id, gruppe, link, titel, beschreibung) VALUES (?, ?, AES_ENCRYPT(?, '$CMS_SCHLUESSEL'), AES_ENCRYPT('', '$CMS_SCHLUESSEL'), AES_ENCRYPT('', '$CMS_SCHLUESSEL'))");
+    $sql->bind_param("iis", $lID, $gID, $link);
+    foreach($ins as $i) {
+      $lID = cms_generiere_kleinste_id("{$gk}links");
+      $gID = $i[0];
+      $link = "https://elearning{$i[1]}.bg-schorndorf.de/{$i[2]}/{$i[3]}";
+      $sql->execute();
+    }
+    $sql->close();
   }
 ?>
