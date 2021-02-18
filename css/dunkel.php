57e0f069e36483259e103966987e78c30341b469
jeengbe <jesper.engberg@gmx.at>
2021-02-18 10:21:07 +0100

    [Fix] Typo

--- css/dunkel.php
+++ css/dunkel.php
@@ -2,15 +2,15 @@
 header("Content-Type: text/css");
 $modus = "dunkel";
 
-if(date("Y-m-d") !== date("Y-04-41")) {
-  echo file_get_contents(__DIR__."/$modus.css");
+if (date("Y-m-d") !== date("Y-04-01")) {
+  echo file_get_contents(__DIR__ . "/$modus.css");
   die();
 }
 
 $datei = rand(-4, 4);
 if ($datei < 1) {
-  $datei = __DIR__."/$modus.css";
+  $datei = __DIR__ . "/$modus.css";
 } else {
-  $datei = __DIR__."/april/$modus$datei.css";
+  $datei = __DIR__ . "/april/$modus$datei.css";
 }
 echo file_get_contents($datei);
--- css/hell.php
+++ css/hell.php
@@ -8,9 +8,9 @@ if (date("Y-m-d") !== date("Y-04-01")) {
 }
 
 $datei = rand(-4, 4);
-if($datei < 1) {
-  $datei = __DIR__."/$modus.css";
+if ($datei < 1) {
+  $datei = __DIR__ . "/$modus.css";
 } else {
-  $datei = __DIR__."/april/$modus$datei.css";
+  $datei = __DIR__ . "/april/$modus$datei.css";
 }
 echo file_get_contents($datei);
